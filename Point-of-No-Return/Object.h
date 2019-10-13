d78eb3d4cc4d61db69e30b8157cba02a07d15f24
shouma0407 <shouma20000407@gmail.com>
2019-10-14 07:38:36 +0900

    主人公のステイタスで必要なラッパークラスを追加

--- Point-of-No-Return/Object.h
+++ Point-of-No-Return/Object.h
@@ -1,65 +1,150 @@
 ﻿#ifndef OPBJECT_H_
 #define OPBJECT_H_
 
+#include "../Point-of-No-Return///Include/DirectX.h"
+#include <iostream>
+#include <vector>
+#include <string>
 
-
-
+//X座標
 class VertexX
 {
-public:
 private:
-
 	float x;
-
 };
-
+//Y座標
 class VertexY
 {
-public:
 private:
-
 	float y;
-
 };
-
+//TU座標
 class VertexTU
 {
-public:
 private:
-
 	float tu;
-
 };
-
+//TV座標
 class VertexTV
 {
-public:
 private:
-
 	float tv;
-
 };
-
+//Color座標
 class VertexColor
 {
-public:
 private:
 	unsigned long color;
 };
 
 
-
+//ヒットポイント
+class HP
+{
+private:
+	int hp;
+};
+//マジックポイント
+class MP
+{
+private:
+	int mp;
+};
+//イマジナリーポイント/想像力
+class IP
+{
+private:
+	int IP;
+};
+//攻撃力
+class Attack
+{
+private:
+	int attack;
+};
+//防御力
+class Defense
+{
+private:
+	int defense;
+};
+//素早さ
+class Speed
+{
+private:
+	int speed;
+};
+//魔法攻撃力
+class MagicAttack
+{
+private:
+	int magic_attack;
+};
+//運
+class Luck
+{
+private:
+	int luck;
+};
+//愛情
+class Love
+{
+	int love;
+};
+//時間
 class Timer
 {
-public:
 private:
 	float time;
 };
+//お金
+class Gold
+{
+private:
+	int gold;
+};
+//年齢
+class Age
+{
+private:
+	int age;
+};
+//名前
+class Name
+{
+private:
+	std::vector<std::string> name;
+};
 
 
+//オブジェクトクラス
 class Object
 {
 public:
+
+	DX::DirectX directx;
+
+	Object()
+	{
+
+	};
+
+	Object(Object& object)
+	{
+
+	};
+
+	~Object()
+	{
+	
+	};
+
+	/*
+	virtual directx.Draw()
+	{
+	
+	};
+	*/
 
 protected:
 
@@ -77,30 +162,54 @@ protected:
 		VertexX x;
 		VertexY y;	
 	};
+
 };
 
 
 
-
+//キャラクタークラス
 class Character :public Object
 {
 
 };
-
+//UIクラス
 class UI:public Object
 {
 
 };
-
+//スクロールステージクラス
 class ScrollStage:public Object
 {
 
 };
-
+//ステージクラス
 class Stage :public Object
 {
 
 };
+
+
+
+
+//主人公クラス
+class Hero :public Character
+{
+
+};
+
+//NPCクラス
+class NPC :public Character
+{
+
+};
+
+//敵クラス
+class Enemy :public Character
+{
+
+};
+
+
 
 #endif
 
