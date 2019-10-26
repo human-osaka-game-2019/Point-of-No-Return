fc025dc0f1a6b4e148e2fc5bdeca91dc0a4637c4
mono1140 <11potato30@gmail.com>
2019-10-27 00:53:13 +0900

    :poop: マップチップの描画に必要な情報をグローバルで作成

--- Point-of-No-Return/Mapchip.h
+++ Point-of-No-Return/Mapchip.h
@@ -8,6 +8,20 @@
 const int map_size_width_pos = 8;
 const int map_size_height_pos = 15;
 
+// ブロックの数
+const int world_size_width_pos = 480;
+const int world_size_height_pos = 16;
+// マップてチップ1つの大きさ
+const int chip_size_ = 64;
+// テクスチャの大きさ
+const int texture_width = 256;
+const int texture_height = 256;
+// 切り取った画像の大きさ
+const int chip_width_num = texture_width / 5;
+const int chip_height_num = texture_height / 5;
+// 描画サイズ
+// const int draw_size = 64;
+
 class DRAWMAP {
 public:
 
