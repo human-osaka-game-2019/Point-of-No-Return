7b35fbccb28db472317ad660370281cce7507072
shouma0407 <shouma20000407@gmail.com>
2019-10-29 15:39:50 +0900

    :bug: loadの読み込み修正

--- Point-of-No-Return/Scene.cpp
+++ Point-of-No-Return/Scene.cpp
@@ -54,34 +54,41 @@ void BaseScene::Load()
 
 	DWORD result;
 
+	int framecount = 0;
 
 	timeBeginPeriod(1);
+	dx.LoadTexture("仮Load.png", "load");
 
 	//Thread関数が処理している間のループ
 	while (true)
 	{
 		Curr = timeGetTime();
 		if (Curr - Prev >= 1000 / 60) {
-
-
+			dx.BeginScene();
+			
+			framecount++;
 			dx.DrawEx(100, 100, 0, 200, 200, 0, 1, 0, "load", 0, 0, 1, 0.25);
+			
 
-
+			
+			
 
 			//スレッドが終わったかチェック
 			GetExitCodeThread(th, &result);
 			//終わったかどうか返す関数
-			if (STILL_ACTIVE != result)
+			if (STILL_ACTIVE != result && framecount == 60*5)
 			{
 				//closehandleで閉じる。
 				CloseHandle(th);
 				//ループを抜ける。
 				break;
 			}
+			dx.EndScene();
 			Prev = Curr;
 		}
 		Sleep(1);
 	}
+	dx.ReleaseTexture("load");
 	timeEndPeriod(1);
 }
 
