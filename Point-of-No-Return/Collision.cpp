997c1a104f169287360e55e19652bc7e3dffbefb
mono1140 <11potato30@gmail.com>
2019-11-01 23:57:07 +0900

    :construction: 当たり判定クラスの作成

--- Point-of-No-Return/Collision.cpp
+++ Point-of-No-Return/Collision.cpp
@@ -1,67 +1,161 @@
 ﻿#include "Collision.h"
 
-int Collision::CheckMapNumber(int x, int y, int** map, int CharDirection)
+int Collision::CheckMapNumber(int x, int y, int** map)
 {
 		
 	return map[y / chip_size_][x / chip_size_];
 	
 }
 
-void Collision::DirectionCheck(Hero hero)
+Direction Collision::DirectionCheckX(Hero hero)
 {
-	// X差分
+	// Xの移動量
 	vec_x = hero.customVertex.x.var - hero.before_x.var;
 
-	// Y差分
-	vec_y = hero.customVertex.y.var - hero.before_y.var;
 
-	if (vec_x > 0)
+	if (vec_x < 0)
 	{
-		direction_x = RIGHT;
+		return RIGHT;
 	}
-	else if(vec_x < 0)
+	else if (vec_x > 0)
 	{
-		direction_x = LEFT;
+		return LEFT;
 	}
-
-	if (vec_y > 0)
-	{
-		direction_y = DOWN;
-	}
-	else if(vec_y < 0)
-	{
-		direction_y = UP;
-	}
-
-	
-
 }
 
+ Direction Collision::DirectionCheckY(Hero hero)
+ {
+	 // Yの移動量
+	 vec_y = hero.customVertex.y.var - hero.before_y.var;
+
+	 if (vec_y < 0)
+	 {
+		 return DOWN;
+	 }
+	 else if (vec_y > 0)
+	 {
+		 return UP;
+	 }
+ }
+ 
 void Collision::HeroAndBlock(Hero hero, int** map)
 {
-	if(CheckMapNumber != AIR)
-	{//右下に移動中/右下の頂点
-		hero.customVertex.x.var - ((int)(hero.customVertex.x.var + hero.customVertex.width.var) / chip_size_) * chip_size_;
-		hero.customVertex.y.var - ((int)(hero.customVertex.y.var + hero.customVertex.height.var) / chip_size_) * chip_size_;
-	}
-	switch (Direction)
+	if (HitPointCheck(hero, map) == true)
 	{
-	case UP:
-
-	
-
-		break;
-	
-	case DOWN:
-
-		break;
-	case RIGHT:
-
-		break;
-	case LEFT:
-
-		break;
+		CoordinateCorrection(hero,map);
 	}
-
 
 }
+
+bool Collision::HitPointCheck(Hero hero,int** map)
+{
+	//	右上 左上 右下 左下
+	if (TopRightHasHit(hero,map) == true || TopLeftHasHit(hero,map) == true || BottomRightHasHit(hero, map) == true || BottomLeftHasHit(hero, map) == true)
+	{	
+		return true;
+	}
+    
+	return false;
+
+}
+
+void Collision::CoordinateCorrection(Hero hero,int** map)
+{
+
+	save_char_move_direction[X] = DirectionCheckX(hero);
+	save_char_move_direction[Y] = DirectionCheckY(hero);
+
+	if (save_char_move_direction[X] == RIGHT && save_char_move_direction[Y] == UP)
+	{
+		if (TopLeftHasHit(hero, map) == true)
+		{
+
+		}
+
+		if (BottomRightHasHit(hero, map) == true)
+		{
+
+		}
+		
+		if (TopRightHasHit(hero, map) == true)
+		{
+
+		}
+
+
+	}
+	else if (save_char_move_direction[X] == RIGHT && save_char_move_direction[Y] == DOWN)
+	{
+
+	}
+
+	if (save_char_move_direction[X] == LEFT && save_char_move_direction[Y] == UP)
+	{
+
+	}
+	else if (save_char_move_direction[X] == LEFT && save_char_move_direction[Y] == DOWN)
+	{
+
+	}
+
+	if (save_char_move_direction[X] == RIGHT)
+	{
+		hero.customVertex.x.var = (hero.customVertex.x.var / chip_size_) * chip_size_ - hero.customVertex.width.var;
+	}
+	else if (save_char_move_direction[X] == LEFT)
+	{
+		hero.customVertex.x.var = (hero.customVertex.x.var / chip_size_) * chip_size_ + chip_size_;
+	}
+
+	if (save_char_move_direction[Y] == UP)
+	{
+		hero.customVertex.y.var = (hero.customVertex.y.var / chip_size_)* chip_size_  + chip_size_;
+	}
+	else if (save_char_move_direction[Y] == DOWN)
+	{
+		hero.customVertex.y.var = (hero.customVertex.y.var / chip_size_) * chip_size_ - hero.customVertex.height.var;
+	}
+
+}
+
+bool Collision::TopRightHasHit(Hero hero, int** map)
+{
+	if (CheckMapNumber((int)(hero.customVertex.x.var + hero.customVertex.width.var), hero.customVertex.y.var, map) == ROAD)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool Collision::TopLeftHasHit(Hero hero, int** map)
+{
+	if (CheckMapNumber(hero.customVertex.x.var, hero.customVertex.y.var, map) == ROAD)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool Collision::BottomRightHasHit(Hero hero, int** map)
+{
+	if (CheckMapNumber((int)(hero.customVertex.x.var + hero.customVertex.width.var), (int)(hero.customVertex.y.var + hero.customVertex.height.var), map) == ROAD)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool Collision::BottomLeftHasHit(Hero hero, int** map)
+{
+	if (CheckMapNumber(hero.customVertex.x.var, (int)(hero.customVertex.y.var + hero.customVertex.height.var), map) == ROAD)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+
--- Point-of-No-Return/Collision.h
+++ Point-of-No-Return/Collision.h
@@ -4,13 +4,14 @@
 #include"Mapchip.h"
 #include"Character.h"
 
-enum 
+enum Direction
 {
+	STOP,
 	UP,
 	DOWN,
 	RIGHT,
 	LEFT,
-	STOP,
+	
 };
 
 enum 
@@ -20,25 +21,59 @@ enum
 
 };
 
+enum
+{
+	X,
+	Y
+};
+
+enum HitPoint
+{
+	TopRight,
+	TopLeft,
+	BottomRight,
+	BottomLeft,
+	NoHit,
+
+};
+
+
 class Collision
 {
 private:
 
 	int vec_x;
 	int vec_y;
-	int direction_x;	
-	int direction_y;
+	// 配列の0番目にUPかDOWNが入り、配列の1番目にRIGHNTかLEFTが入る
+	int save_char_move_direction[2] = {0};
 
+     // マップチップの中身(数)を返す関数
+	int CheckMapNumber(int x,int y, int** map);
 
 public:
-	// マップチップの中身(数)を返す関数
-	int CheckMapNumber(int x,int y, int** map, int CharDirection);
+	
 
-	// 方向を算出する関数
-	void DirectionCheck(Hero hero);
+	// Xの方向を算出する関数
+	Direction DirectionCheckX(Hero hero);
+	// Yの方向を算出する関数
+	Direction DirectionCheckY(Hero hero);
 
 	// 当たり判定関数
 	void HeroAndBlock(Hero hero, int** map);
+
+	// 当たったかの確認関数
+	// 当たっていればtureを、当たっていなければ false を返す
+	bool HitPointCheck(Hero hero,int** map);
+
+	void CoordinateCorrection(Hero hero,int** map);
+
+	bool TopRightHasHit(Hero hero, int** map);
+
+	bool TopLeftHasHit(Hero hero, int** map);
+
+	bool BottomRightHasHit(Hero hero, int** map);
+
+	bool BottomLeftHasHit(Hero hero,int** map);
 };
 
 
