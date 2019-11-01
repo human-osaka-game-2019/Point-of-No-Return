89966cb3ab23eea90ed89b1bedc70ec8bb870327
mono1140 <11potato30@gmail.com>
2019-11-01 12:45:19 +0900

    :art: .cpp にクラスが書かれているのを .h に移動

--- Point-of-No-Return/Character.h
+++ Point-of-No-Return/Character.h
@@ -1,7 +1,60 @@
 ﻿#ifndef CHARCTER_H_
 #define CHARCTER_H_
 
+#include "Object.h"
 
+//キャラクタークラス
+class Character :public Object
+{
+
+};
+
+//UIクラス
+class UI :public Object
+{
+
+};
+
+//スクロールステージクラス
+class ScrollStage :public Object
+{
+
+};
+
+//ステージクラス
+class Stage :public Object
+{
+
+};
+
+
+
+
+//主人公クラス
+class Hero :public Character
+{
+public:
+	CustomVertex customVertex;
+	VertexX before_x;
+	VertexX before_y;
+
+	// 仮置き
+	int x_move_speed;
+	int y_move_speed;
+
+};
+
+//NPCクラス
+class NPC :public Character
+{
+
+};
+
+//敵クラス
+class Enemy :public Character
+{
+
+};
 
 
 
