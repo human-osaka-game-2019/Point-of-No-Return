21c8b6e75bd955103df43b76710b32d0eb457bfc
mono1140 <11potato30@gmail.com>
2019-11-01 12:48:03 +0900

    :sparkles: Collision.cpp Collision.h の追加

---
+++ Point-of-No-Return/Collision.cpp
@@ -0,0 +1,67 @@
+﻿#include "Collision.h"
+
+int Collision::CheckMapNumber(int x, int y, int** map, int CharDirection)
+{
+		
+	return map[y / chip_size_][x / chip_size_];
+	
+}
+
+void Collision::DirectionCheck(Hero hero)
+{
+	// X差分
+	vec_x = hero.customVertex.x.var - hero.before_x.var;
+
+	// Y差分
+	vec_y = hero.customVertex.y.var - hero.before_y.var;
+
+	if (vec_x > 0)
+	{
+		direction_x = RIGHT;
+	}
+	else if(vec_x < 0)
+	{
+		direction_x = LEFT;
+	}
+
+	if (vec_y > 0)
+	{
+		direction_y = DOWN;
+	}
+	else if(vec_y < 0)
+	{
+		direction_y = UP;
+	}
+
+	
+
+}
+
+void Collision::HeroAndBlock(Hero hero, int** map)
+{
+	if(CheckMapNumber != AIR)
+	{//右下に移動中/右下の頂点
+		hero.customVertex.x.var - ((int)(hero.customVertex.x.var + hero.customVertex.width.var) / chip_size_) * chip_size_;
+		hero.customVertex.y.var - ((int)(hero.customVertex.y.var + hero.customVertex.height.var) / chip_size_) * chip_size_;
+	}
+	switch (Direction)
+	{
+	case UP:
+
+	
+
+		break;
+	
+	case DOWN:
+
+		break;
+	case RIGHT:
+
+		break;
+	case LEFT:
+
+		break;
+	}
+
+
+}
---
+++ Point-of-No-Return/Collision.h
@@ -0,0 +1,46 @@
+﻿#ifndef COLLISION
+#define COLLISION
+
+#include"Mapchip.h"
+#include"Character.h"
+
+enum 
+{
+	UP,
+	DOWN,
+	RIGHT,
+	LEFT,
+	STOP,
+};
+
+enum 
+{ 
+	AIR,
+	ROAD,
+
+};
+
+class Collision
+{
+private:
+
+	int vec_x;
+	int vec_y;
+	int direction_x;	
+	int direction_y;
+
+
+public:
+	// マップチップの中身(数)を返す関数
+	int CheckMapNumber(int x,int y, int** map, int CharDirection);
+
+	// 方向を算出する関数
+	void DirectionCheck(Hero hero);
+
+	// 当たり判定関数
+	void HeroAndBlock(Hero hero, int** map);
+};
+
+
+
+#endif
