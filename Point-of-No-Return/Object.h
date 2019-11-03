e2c444e4b9bbcbaeaab9fb6d16054f9d493b28cf
OhuraKazuma <k.ohura0303@gmail.com>
2019-11-03 14:23:26 +0900

    [#13]:art: ラップした構造体を最適化。それらで構成されたクラスを作成。

--- Point-of-No-Return/Object.h
+++ Point-of-No-Return/Object.h
@@ -9,143 +9,190 @@
 
 
 //X座標
-class VertexX
+struct VertexX
 {
-private:
-	float x;
-};
-//Y座標
-class VertexY
-{
-private:
-	float y;
-};
-//TU座標
-class VertexTU
-{
-private:
-	float tu;
-};
-//TV座標
-class VertexTV
-{
-private:
-	float tv;
-};
-//Color座標
-class VertexColor
-{
-private:
-	unsigned long color;
+	VertexX(float value) :value(value) {}
+	float value;
 };
 
+//Y座標
+struct VertexY
+{
+	VertexY(float value) :value(value) {}
+	float value;
+};
+
+//横幅
+struct Width
+{
+	Width(float value) :value(value) {}
+	float value;
+};
+//縦幅
+struct Height
+{
+	Height(float value) :value(value) {}
+	float value;
+};
+
+//TU座標
+struct VertexTU
+{
+	VertexTU(float value) :value(value) {}
+	float value;
+};
+
+//TV座標
+struct VertexTV
+{
+	VertexTV(float value) :value(value) {}
+	float value;
+};
 
 //ヒットポイント
-class HP
+struct HP
 {
-private:
-	int hp;
+	HP(int value) :value(value) {}
+	int value;
 };
+
 //マジックポイント
-class MP
+struct MP
 {
-private:
-	int mp;
+	MP(int value) :value(value) {}
+	int value;
 };
+
 //イマジナリーポイント/想像力
-class IP
+struct IP
 {
-private:
-	int IP;
+	IP(int value) :value(value) {}
+	int value;
 };
+
 //攻撃力
-class Attack
+struct Attack
 {
-private:
-	int attack;
+	Attack(int value) :value(value) {}
+	int value;
 };
+
 //防御力
-class Defense
+struct Defense
 {
-private:
-	int defense;
+	Defense(int value) :value(value) {}
+	int value;
 };
+
 //素早さ
-class Speed
+struct Speed
 {
-private:
-	int speed;
+	Speed(int value) :value(value) {}
+	int value;
 };
+
 //魔法攻撃力
-class MagicAttack
+struct MagicAttack
 {
-private:
-	int magic_attack;
+	MagicAttack(int value) :value(value) {}
+	int value;
 };
+
 //運
-class Luck
+struct Luck
 {
-private:
-	int luck;
+	Luck(int value) :value(value) {}
+	int value;
 };
+
 //愛情
-class Love
+struct Love
 {
-	int love;
+	Love(int value) :value(value) {}
+	int value;
 };
+
 //時間
-class Timer
+struct Time
 {
-private:
-	float time;
+	Time(int value) :value(value) {}
+	int value;
 };
+
 //お金
-class Gold
+struct Gold
 {
-private:
-	int gold;
+	Gold(int value) :value(value) {}
+	int value;
 };
+
 //年齢
-class Age
+struct Age
 {
-private:
-	int age;
+	Age(int value) :value(value) {}
+	int value;
 };
+
 //名前
-class Name
+struct Name
 {
-private:
-	std::vector<std::string> name;
+	Name(std::string value) :value(value) {}
+	std::string value;
 };
+
+struct Vec2
+{
+	VertexX x;
+	VertexY y;
+
+	Vec2(float x, float y) : x(x),y(y) {};
+};
+
+struct Size
+{
+	Width width;
+	Height height;
+
+	Size(float width,float height) : width(width), height(height) {};
+};
+
+struct TextureUV
+{
+	VertexTU tu;
+	VertexTV tv;
+
+	TextureUV(float tu,float tv) : tu(tu), tv(tv) {};
+};
+
+struct TextureSize
+{
+	Width width;
+	Height height;
+
+	TextureSize(float width,float height) : width(width), height(height) {};
+};
+
 
 
 //オブジェクトクラス
 class Object
 {
 public:
-	
-	/*
-	virtual directx.Draw()
-	{
-	
-	};
-	*/
+
+	virtual void Draw() = 0;
 
 protected:
+	DX::DirectX& dx = DX::DirectX::GetInstance();
 
 	struct CustomVertex
 	{
-		VertexX x;
-		VertexY y;
-		VertexColor color;
-		VertexTU tu;
-		VertexTV tv;
-	};
+		Vec2 vec;
+		Size size;
+		TextureUV uv;
+		TextureSize texture_size;
 
-	struct Vec2
-	{
-		VertexX x;
-		VertexY y;	
+		CustomVertex(float x, float y, float width, float hieght, float tu, float tv, float texture_width, float texture_height) : vec(x, y), size(100, 100), uv(0, 0), texture_size(1.0, 1.0) {};
+
 	};
 
 };
