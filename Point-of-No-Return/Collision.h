daae723d1402a9853a88ede444869029a1ef3680
mono1140 <11potato30@gmail.com>
2019-11-06 19:00:53 +0900

    :pencil: Doxygen コメントの追加

--- Point-of-No-Return/Collision.h
+++ Point-of-No-Return/Collision.h
@@ -55,43 +55,123 @@ private:
 	// 配列の0番目にUPかDOWNが入り、配列の1番目にRIGHNTかLEFTが入る
 	int save_char_move_direction[2] = {0};
 
-     // マップチップの中身(数)を返す関数
+
+
+	/**
+	 * マップチップの二次元配列の中身を返す関数
+	 * @param 対象のX座標
+	 * @param 対象のY座標
+	 * @param map情報のアドレス
+	 * @return　マップチップの二次元配列の中身
+	 */
 	int CheckMapNumber(int x,int y, int** map);
 
 public:
 	
+	/**
+     * X軸の動きの方向を算出する関数
+     * @param 対象のX座標
+     * @param 対象の1フレーム前のX座標
+     * @return Direction型の Right Left
+     */
+	Direction VerticalDirectionCheck(int x, int before_x);
 
-	// Xの方向を算出する関数
-	Direction VerticalDirectionCheck(Hero hero);
+	/**
+	 * Y軸の動きの方向を算出する関数
+	 * @param 対象のY座標
+	 * @param 対象の1フレーム前のY座標
+	 * @return Direction型の Up Down
+	 */
+	Direction HorizontalDirectionCheck(int y, int before_y);
 
-	// Yの方向を算出する関数
-	Direction HorizontalDirectionCheck(Hero hero);
+	/**
+     * ブロックとの当たり判定の関数
+     * @param 対象のX座標
+	 * @param 対象のX軸の描画サイズ
+     * @param 対象のY座標
+	 * @param 対象のY軸の描画サイズ
+	 * @param 対象の1フレーム前のX座標
+	 * @param 対象の1フレーム前のY座標
+     * @param mapのアドレス
+     */
+	void ブロックとの(int x, int x_size, int y, int y_size, int before_x, int before_y, int** map);
 
-	// 当たり判定関数
-	void HeroAndBlock(Hero hero, int** map);
+	/**
+	 * 各頂点を見て当たっているかどうかの確認関数
+	 * @param 対象のX座標
+	 * @param 対象のX軸の描画サイズ
+	 * @param 対象のY座標
+　　 * @param 対象のY軸の描画サイズ
+	 * @param mapのアドレス
+	 */
+	bool HitPointCheck(int x, int x_size, int y, int y_size,int** map);
 
-	// 当たったかの確認関数
-	// 当たっていればtureを、当たっていなければ false を返す
-	bool HitPointCheck(Hero hero,int** map);
+	/**
+	 * どの方向に動いているかを判別する関数
+	 * @param 対象のX座標
+	 * @param 対象のX軸の描画サイズ
+	 * @param 対象のY座標
+　　 * @param 対象のY軸の描画サイズ
+	 * @param mapのアドレス
+	 */
+	void DirectionCheck(int x, int x_size, int y, int y_size, int before_x, int before_y,int** map);
 
-	// ブロックにめり込んでいた場合、
-	void DirectionCheck(Hero hero,int** map);
-
-	// 座標を修正する関数
-	void CoordinateCorrection(Hero hero,int direction);
+	/**
+	 * 座標を修正する関数
+	 * @param 対象のX座標
+	 * @param 対象のX軸の描画サイズ
+	 * @param 対象のY座標
+　　 * @param 対象のY軸の描画サイズ
+     * @param 対象の動いている方向
+	 */
+	void CoordinateCorrection(int x, int x_size, int y, int y_size,int direction);
 	
-	// 右上の頂点がブロックにあたっているかどうか
-	bool TopRightHasHit(Hero hero, int** map);
+	/**
+	 * 右上の頂点がブロックにあたっているかどうかを判別する関数
+	 * @param 対象のX座標
+	 * @param 対象のX軸の描画サイズ
+	 * @param 対象のY座標
+	 * @return 当たっていたら true を返す
+	 */
+	bool TopRightHasHit(int x, int x_size, int y, int** map);
 
-	// 左上の頂点がブロックにあたっているかどうか
-	bool TopLeftHasHit(Hero hero, int** map);
+	/**
+	 * 左上の頂点がブロックにあたっているかどうかを判別する関数
+	 * @param 対象のX座標
+	 * @param 対象のY座標
+	 * @return 当たっていたら true を返す
+	 */
+	bool TopLeftHasHit(int x, int y, int** map);
 
-	// 右下の頂点がブロックにあたっているかどうか
-	bool BottomRightHasHit(Hero hero, int** map);
+	/**
+	 * 右下の頂点がブロックにあたっているかどうかを判別する関数
+	 * @param 対象のX座標
+	 * @param 対象のX軸の描画サイズ
+	 * @param 対象のY座標
+　　 * @param 対象のY軸の描画サイズ
+	 * @param mapのアドレス
+	 * @return 当たっていたら true を返す
+	 */
+	bool BottomRightHasHit(int x, int x_size, int y, int y_size, int** map);
 
-	// 左下の頂点がブロックにあたっているかどうか
-	bool BottomLeftHasHit(Hero hero,int** map);
 
+	/**
+	 * 左下の頂点がブロックにあたっているかどうかを判別する関数
+	 * @param 対象のX座標
+	 * @param 対象のY座標
+　　 * @param 対象のY軸の描画サイズ
+	 * @param mapのアドレス
+	 * @return 当たっていたら true を返す
+	 */
+	bool BottomLeftHasHit(int x, int y, int y_size,int** map);
+
+
+	/**
+	 * 斜めにめり込んだ時の当たり判定をどこの辺でとるのかを判別する関数
+	 * @param 対象のX座標
+	 * @param 対象のY座標
+	 * @return どの当たり判定を使用するのかを返す
+	 */
 	int HitVectorJudge(int X, int Y, int move_deflection);
 };
 
