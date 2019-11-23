fd71833305ee5e4d88a7302ace71beb6adc1c40f
taishi-matsumura <taishi0706@gmail.com>
2019-11-24 03:38:02 +0900

    :recycle: 不要そうな引数を削除

--- Point-of-No-Return/Mapchip.cpp
+++ Point-of-No-Return/Mapchip.cpp
@@ -4,14 +4,14 @@
 #include "Main.h"
 #include "string"
 
-void Mapchip::DrawMapchip(int map_size_width, int map_size_height, float texture_width, float texture_height, float mapchip_width, float mapchip_height, float draw_width, float draw_height, float draw_pos_x, float draw_pos_y, std::string texturename, int** map)
+void Mapchip::DrawMapchip(float draw_start_pos_x, float draw_start_pos_y, std::string texturename, int** map)
 {
-	int width_num = texture_width / mapchip_width;
-	int height_num = texture_height / mapchip_height;
+	int width_num = TEXTURE_WIDTH / CHIP_WIDTH_NUM;
+	int height_num = TEXTURE_HEIGHT / CHIP_HEIGHT_NUM;
 
-	for (int i = 0; i < map_size_height; i++)
+	for (int i = 0; i < WORLD_SIZE_HEIGHT; i++)
 	{
-		for (int j = 0; j < map_size_width; j++)
+		for (int j = 0; j < WORLD_SIZE_WIDTH; j++)
 		{
 			int chip_id = map[i][j];
 			if (chip_id == 0)
@@ -19,20 +19,24 @@ void Mapchip::DrawMapchip(int map_size_width, int map_size_height, float texture
 				continue;
 			}
 
-			float chip_pos_x = (float)(chip_id % width_num) * mapchip_width;
-			float chip_pos_y = (float)(chip_id / height_num) * mapchip_height;
-
-			TexturePrint(draw_pos_x + draw_width * j, draw_pos_y + draw_height * i, draw_width, draw_height, chip_pos_x / texture_width, chip_pos_y / texture_height, mapchip_width / texture_width, mapchip_height / texture_height, texturename);
+			float draw_pos_x = draw_start_pos_x + CHIP_SIZE * j;
+			float draw_pos_y = draw_start_pos_y + CHIP_SIZE * i;
+			float chip_pos_x = (float)(chip_id % width_num) * CHIP_WIDTH_NUM / TEXTURE_WIDTH;
+			float chip_pos_y = (float)(chip_id / height_num) * CHIP_HEIGHT_NUM / TEXTURE_HEIGHT;
+			TexturePrint(draw_pos_x, draw_pos_y, chip_pos_x, chip_pos_y, texturename);
 		}
 	}
 }
 
 //マップチップ描画
-void Mapchip::TexturePrint(int drawpos_x, int drawpos_y, int mapcip_width, int mapchip_height, float chip_pos_x, float chip_pos_y, float width_num, float height_num, std::string texturename)
+void Mapchip::TexturePrint(float drawpos_x, float drawpos_y, float chip_pos_x, float chip_pos_y, std::string texturename)
 {
 	DirectX& dx = DirectX::GetInstance();
 
-	dx.DrawEx(drawpos_x, drawpos_y, 0, mapcip_width, mapchip_height, 0, 1.f, 0, texturename, chip_pos_x, chip_pos_y, width_num, height_num);
+	float width_num = static_cast<float>(CHIP_WIDTH_NUM) / TEXTURE_WIDTH;
+	float height_num = static_cast<float>(CHIP_HEIGHT_NUM) / TEXTURE_HEIGHT;
+
+	dx.DrawEx(drawpos_x, drawpos_y, 0, CHIP_SIZE, CHIP_SIZE, 0, 1.f, false, texturename, chip_pos_x, chip_pos_y, width_num, height_num);
 }
 
 
--- Point-of-No-Return/Mapchip.h
+++ Point-of-No-Return/Mapchip.h
@@ -26,7 +26,7 @@ public:
 	 * @param  
 	 * @details 
 	 */
-	void DrawMapchip(int map_size_width, int map_size_height, float texture_width, float texture_height, float mapchip_width, float mapchip_height, float draw_width, float draw_height, float draw_pos_x, float draw_pos_y, std::string texturename, int** map);
+	void DrawMapchip(float draw_start_pos_x, float draw_start_pos_y, std::string texturename, int** map);
 
 	/**
 	 * @brief  
@@ -35,7 +35,7 @@ public:
 	 * @param  
 	 * @details 
 	 */
-	void TexturePrint(int drawpos_x, int drawpos_y, int mapcip_width, int mapchip_height, float chip_pos_x, float chip_pos_y, float width_num, float height_num, std::string texturename);
+	void TexturePrint(float drawpos_x, float drawpos_y, float chip_pos_x, float chip_pos_y, std::string texturename);
 
 	/**
 	 * @brief	マップの初期化
