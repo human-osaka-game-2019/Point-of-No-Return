02c37eb2316d299bf325cec2cfe029fc84a0a781
taishi-matsumura <taishi0706@gmail.com>
2019-11-27 10:38:49 +0900

    :recycle: CoordinateX/Yに関する比較演算子をオーバーロード(#5)

---
+++ PoNR-Test/ObjectTest.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+
+#include "../Point-of-No-Return/Object.h"
+
+TEST(ObjectTest, LessThan_1) {
+	// Arrange
+	CoordinateX x(0.5f);
+
+	// Act
+	bool result = (x < CoordinateX(0.6f));
+
+	// Assert
+	EXPECT_TRUE(result);
+}
+
+TEST(ObjectTest, LessThan_2) {
+	// Arrange
+	CoordinateX x(0.5f);
+
+	// Act
+	bool result = (x < CoordinateX(0.4f));
+
+	// Assert
+	EXPECT_FALSE(result);
+}
+
+TEST(ObjectTest, LessThan_3) {
+	// Arrange
+	CoordinateX x(0.5f);
+
+	// Act
+	bool result = (x < CoordinateX(0.5f));
+
+	// Assert
+	EXPECT_FALSE(result);
+}
