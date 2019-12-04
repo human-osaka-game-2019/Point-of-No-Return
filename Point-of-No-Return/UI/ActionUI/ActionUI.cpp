d16acbfdc5edb4fd3ee98426076db1f5b6cd3507
mono1140 <11potato30@gmail.com>
2019-12-04 21:45:32 +0900

    :art: 画像の名前の変更、描画のtu,tvの値の修正、Bar,Frameクラスの実装

--- Point-of-No-Return/UI/ActionUI/ActionUI.cpp
+++ Point-of-No-Return/UI/ActionUI/ActionUI.cpp
@@ -23,17 +23,16 @@ namespace ActionUI
 	//	default:
 	//		break;
 	//	}
-
 	//	float drow_size = (size.width.value / 100) * value;
-
-
 	//}
+
+	// ----------- ItemUI ---------------------------------------------------------------------------------------------------------
 
 	void ItemUI::Initialize()
 	{
 		position =
 		{
-			CoordinateX(0),
+			CoordinateX(576),
 			CoordinateY(0)
 		};
 
@@ -53,8 +52,8 @@ namespace ActionUI
 		};
 
 
-		float tu_size = 642.f / 2048.f;
-		float tv_size = 228.f / 514.f;
+		float tu_size = 512.f / 2048.f;
+		float tv_size = 128.f / 514.f;
 
 		texture_size =
 		{
@@ -62,24 +61,36 @@ namespace ActionUI
 			Height(tv_size)
 		};
 
-		texture_name = TextureName("Action");
+		texture_name = TextureName("ActionUI");
 		degree = Degree(0);
 		zoom = Zoom(1.0);
 		is_reverse = IsReverse(false);
 	}
 
+	void ItemUI::Draw()
+	{
+		dx.DrawEx(position.x.value, position.y.value, 0, size.width.value, size.height.value, degree.value, zoom.value, is_reverse.value, texture_name.value, uv.tu.value, uv.tv.value, texture_size.width.value, texture_size.height.value);
+	}
+
+	void ItemUI::UpDate()
+	{
+
+	}
+
+	// ----------- MoneyUI ---------------------------------------------------------------------------------------------------------
+
 	void MoneyUI::Initialize()
 	{
 		position =
 		{
-			CoordinateX(0),
+			CoordinateX(1088),
 			CoordinateY(0)
 		};
 
 		size =
 		{
-			Width(128),
-			Height(128)
+			Width(100),
+			Height(100)
 		};
 
 		float tv = 100.f / 514.f;
@@ -91,7 +102,7 @@ namespace ActionUI
 		};
 
 		float tu_size = 128.f / 2048.f;
-		float tv_size = 228.f / 2048.f;
+		float tv_size = 128.f / 514.f;
 
 		texture_size =
 		{
@@ -99,13 +110,37 @@ namespace ActionUI
 			Height(tv_size)
 		};
 
-		texture_name = TextureName("Action");
+		texture_name = TextureName("ActionUI");
 		degree = Degree(0);
 		zoom = Zoom(1.0);
 		is_reverse = IsReverse(false);
 	}
 
-	void HpUI::Initialize()
+	void MoneyUI::Draw()
+	{
+		dx.DrawEx(position.x.value, position.y.value, 0, size.width.value, size.height.value, degree.value, zoom.value, is_reverse.value, texture_name.value, uv.tu.value, uv.tv.value, texture_size.width.value, texture_size.height.value);
+	}
+
+	// ----------- HpGauge ---------------------------------------------------------------------------------------------------------
+
+	void HpGauge::Initialize()
+	{
+		hpBar.Initialize();
+		hpFrame.Initialize();
+	}
+
+	void HpGauge::UpDate()
+	{
+		hpBar.UpDate();
+	}
+
+	void HpGauge::Draw()
+	{
+		hpBar.Draw();
+		hpFrame.Draw();
+	}
+
+	void HpGauge::HpFrame::Initialize()
 	{
 		position =
 		{
@@ -134,14 +169,85 @@ namespace ActionUI
 			Height(tv_size)
 		};
 
-		texture_name = TextureName("Action");
+		texture_name = TextureName("ActionUI");
 		degree = Degree(0);
 		zoom = Zoom(1.0);
 		is_reverse = IsReverse(false);
 
 	}
 
-	void MpUI::Initialize()
+	void HpGauge::HpFrame::Draw()
+	{
+		dx.DrawEx(position.x.value, position.y.value, 0, size.width.value, size.height.value, degree.value, zoom.value, is_reverse.value, texture_name.value, uv.tu.value, uv.tv.value, texture_size.width.value, texture_size.height.value);
+	}
+
+	void HpGauge::HpBar::Initialize()
+	{
+		position =
+		{
+			CoordinateX(0),
+			CoordinateY(0)
+		};
+
+		size =
+		{
+			Width(576),
+			Height(48)
+		};
+
+		float tv = 50.f / 514.f;
+
+		uv =
+		{
+			TextureU(0),
+			TextureV(tv)
+		};
+
+		float tu_size = 576.f / 2048.f;
+		float tv_size = 48.f / 514.f;
+
+		texture_size =
+		{
+			Width(tu_size),
+			Height(tv_size)
+		};
+
+		texture_name = TextureName("ActionUI");
+		degree = Degree(0);
+		zoom = Zoom(1.0);
+		is_reverse = IsReverse(false);
+	}
+
+	void HpGauge::HpBar::Draw()
+	{
+		dx.DrawEx(position.x.value, position.y.value, 0, size.width.value, size.height.value, degree.value, zoom.value, is_reverse.value, texture_name.value, uv.tu.value, uv.tv.value, texture_size.width.value, texture_size.height.value);
+	}
+
+	void HpGauge::HpBar::UpDate()
+	{
+		// float drow_size = (size.width.value / 100) * status.hp.value;
+	}
+
+	// ----------- MpGauge ---------------------------------------------------------------------------------------------------------
+
+	void MpGauge::Initialize()
+	{
+		mpFrame.Initialize();
+		mpBar.Initialize();
+	}
+
+	void MpGauge::UpDate()
+	{
+		mpBar.UpDate();
+	}
+
+	void MpGauge::Draw()
+	{
+		mpBar.Draw();
+		mpFrame.Draw();
+	}
+
+	void MpGauge::MpFrame::Initialize()
 	{
 		position =
 		{
@@ -163,7 +269,7 @@ namespace ActionUI
 			TextureV(0)
 		};
 
-		float tu_size = 1154.f / 2048.f;
+		float tu_size = 576.f / 2048.f;
 		float tv_size = 48.f / 514;
 
 		texture_size =
@@ -172,13 +278,85 @@ namespace ActionUI
 			Height(tv_size)
 		};
 
-		texture_name = TextureName("Action");
+		texture_name = TextureName("ActionUI");
 		degree = Degree(0);
 		zoom = Zoom(1.0);
 		is_reverse = IsReverse(false);
 	}
 
-	void IpUI::Initialize()
+	void MpGauge::MpFrame::Draw()
+	{
+		dx.DrawEx(position.x.value, position.y.value, 0, size.width.value, size.height.value, degree.value, zoom.value, is_reverse.value, texture_name.value, uv.tu.value, uv.tv.value, texture_size.width.value, texture_size.height.value);
+	}
+
+	void MpGauge::MpBar::Initialize()
+	{
+		position =
+		{
+			CoordinateX(0),
+			CoordinateY(48)
+		};
+
+		size =
+		{
+			Width(576),
+			Height(48)
+		};
+
+		float tu = 578.f / 2048.f;
+		float tv = 50.f / 514.f;
+
+		uv =
+		{
+			TextureU(tu),
+			TextureV(tv)
+		};
+
+		float tu_size = 576.f / 2048.f;
+		float tv_size = 48.f / 514.f;
+
+		texture_size =
+		{
+			Width(tu_size),
+			Height(tv_size)
+		};
+
+		texture_name = TextureName("ActionUI");
+		degree = Degree(0);
+		zoom = Zoom(1.0);
+		is_reverse = IsReverse(false);
+	}
+
+	void MpGauge::MpBar::Draw()
+	{
+		dx.DrawEx(position.x.value, position.y.value, 0, size.width.value, size.height.value, degree.value, zoom.value, is_reverse.value, texture_name.value, uv.tu.value, uv.tv.value, texture_size.width.value, texture_size.height.value);
+	}
+
+	void MpGauge::MpBar::UpDate()
+	{
+		// float drow_size = (size.width.value / 100) * status.mp.value;
+	}
+
+	// ----------- IpGauge ---------------------------------------------------------------------------------------------------------
+
+	void IpGauge::Initialize()
+	{
+		ipFrame.Initialize();
+		ipBar.Initialize();
+	}
+
+	void IpGauge::UpDate()
+	{
+		ipBar.UpDate();
+	}
+
+	void IpGauge::Draw()
+	{
+		ipBar.Draw();
+		ipFrame.Draw();
+	}
+
+	void IpGauge::IpFrame::Initialize()
 	{
 		position =
 		{
@@ -200,7 +378,7 @@ namespace ActionUI
 			TextureV(0)
 		};
 
-		float tu_size = 1732.f / 2048.f;
+		float tu_size = 576.f / 2048.f;
 		float tv_size = 48.f / 514;
 
 		texture_size =
@@ -209,9 +387,63 @@ namespace ActionUI
 			Height(tv_size)
 		};
 
-		texture_name = TextureName("Action");
+		texture_name = TextureName("ActionUI");
 		degree = Degree(0);
 		zoom = Zoom(1.0);
 		is_reverse = IsReverse(false);
 	}
+
+	void IpGauge::IpFrame::Draw()
+	{
+		dx.DrawEx(position.x.value, position.y.value, 0, size.width.value, size.height.value, degree.value, zoom.value, is_reverse.value, texture_name.value, uv.tu.value, uv.tv.value, texture_size.width.value, texture_size.height.value);
+	}
+
+	void IpGauge::IpBar::Initialize()
+	{
+		position =
+		{
+			CoordinateX(0),
+			CoordinateY(96)
+		};
+
+		size =
+		{
+			Width(576),
+			Height(48)
+		};
+
+		float tu = 1156.f / 2048.f;
+		float tv = 50.f / 514.f;
+
+		uv =
+		{
+			TextureU(tu),
+			TextureV(tv)
+		};
+
+		float tu_size = 576.f / 2048.f;
+		float tv_size = 48.f / 514;
+
+		texture_size =
+		{
+			Width(tu_size),
+			Height(tv_size)
+		};
+
+		texture_name = TextureName("ActionUI");
+		degree = Degree(0);
+		zoom = Zoom(1.0);
+		is_reverse = IsReverse(false);
+	}
+
+	void IpGauge::IpBar::Draw()
+	{
+		dx.DrawEx(position.x.value, position.y.value, 0, size.width.value, size.height.value, degree.value, zoom.value, is_reverse.value, texture_name.value, uv.tu.value, uv.tv.value, texture_size.width.value, texture_size.height.value);
+	}
+
+	void IpGauge::IpBar::UpDate()
+	{
+		// float drow_size = (size.width.value / 100) * status.ip.value;
+	}
+
 }
